c4a7af48fdc77847d2530a0e04f06394ac2c68c3
Redouane Moudjaoui <moudjored@gmail.com>
2026-03-13 17:34:07 +0100

    added some libraries (I know I know that isn't really anything)

---
+++ client.c
@@ -0,0 +1,8 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+
+int main(void){
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,9 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+
+int main(void){
+	struct sockaddr_in sa;
+	return 0;
+}
